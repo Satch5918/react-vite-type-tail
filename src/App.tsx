f695ae5f9d9779dcb2c38632051a927e2569db1f
Satch5918 <satch_elfante@hotmail.com>
2023-04-22 22:13:04 -0300

    second

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import goku from './assets/descarga.jpg'
 
-function App() {
-  const [count, setCount] = useState(0)
+interface CardProps {
+  title: string
+  description: string
+  image: string
+}
 
+const App: React.FC<CardProps> = ({ title, description, image }) => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <img className="w-full" src={goku} alt={title} />
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">asdasda</div>
+        <p className="text-gray-700 text-base">rtrt56435</p>
       </div>
-     <h1 className="text-3xl font-bold underline text-center">Hello world!</h1> 
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
